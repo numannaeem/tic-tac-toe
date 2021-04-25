b36cf7b69d1f9a71ebccbf5cd74f16d0b8f9e353
numannaeem <numan.naeem9@gmail.com>
2021-04-26 01:33:49 +0530

    Board works!

--- scripts.js
+++ scripts.js
@@ -1,35 +1,66 @@
-$(".board-cell").data('selected',false)
-var turn = 1
+var turn = 'X'
+var gameWinner = ""
+var gameState= ["","","","","","","","",""]
 
-function startgame() {
-    if (turn === 1) {
-        play('X')
+$('.board-cell').hover(
+    function() { 
+        if (gameState[$(this).data('id')] == '') {
+            // $(this).css("opacity", 0)
+            // setTimeout(() => $(this).css("opacity", 1) , 300)
+            $(this).addClass('hover ' + turn)
+        }
+    },
+    function() { 
+        if (gameState[$(this).data('id')] == '')
+            $(this).removeClass('hover '+ turn) 
     }
-    else if (turn === 2) {
-        play('O')
+)
+
+$('.board-cell').click(function() {
+    if (gameState[$(this).data('id')] == '') {
+        $(this).addClass(turn)
+        $(this).removeClass('hover')
+        gameState[$(this).data('id')] = turn
+        checkwin();
     }
-    if(false) {
-        
+})
+
+function checkwin() {
+    let winningPositions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    for(let i = 0; i<8; i++) {
+        let position = winningPositions[i]
+        let a = gameState[position[0]];
+        let b = gameState[position[1]];
+        let c = gameState[position[2]];
+        if(a=='' || b=='' || c=='')
+            continue;
+        if (a==b && b==c)
+            gameWinner = turn
     }
+    if (!gameState.includes(''))
+        gameWinner = 'draw'
+    console.log(gameWinner)
+    if(gameWinner != "")
+        winner(gameWinner)
+    else changePlayer()
 }
-
-function play(ch) {
-    $('.board-cell').hover(
-        function() { 
-            if ($(this).data('selected') == false) {
-                $(this).addClass('hover ' + ch)
-                $(this).on('click', function() {
-                    turn = (turn%2) + 1
-                    console.log(turn)
-                    $(this).removeClass('hover')
-                    $(this).data('selected',true)
-                })
-            }
-        },
-        function() { 
-            if ($(this).data('selected') == false)
-                $(this).removeClass('hover ' + ch) 
-        })
+function changePlayer() {
+    turn = (turn == 'X')? 'O' : 'X'
+    $('#player-text').text("Player " + turn +"'s turn")
 }
-
-startgame()
+function winner(player) {
+    $('.board-cell').off();
+    let winnertext= player + " wins! :)"
+    if(player == 'draw') 
+        winnertext = "It's a draw :/"
+    $('#player-text').text(winnertext)
+}
